42672715e512eb3f8ac4e5a8343af17f569e0cc6
Marcus21RN <0323105847@ut-tijuana.edu.mx>
2025-07-19 23:27:50 -0700

    Refactor backend structure and update imports
    Moved reservation-related controllers, models, and routes into a new clientBackend directory for better organization. Updated import paths in event routes and server entry point to reflect the new structure. Added .env to .gitignore to prevent sensitive data from being committed.

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
+
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
@@ -14,4 +15,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import eventRoutes from './routes/events.js';
-import reservationRoutes from './routes/reservations.js';
+import eventRoutes from './clientBackend/routes/events.js';
+import reservationRoutes from './clientBackend/routes/reservations.js';
 import connectDB from './config/db.js';
 import authRoutes from './userBackend/routes/authRoutes.js';
 
