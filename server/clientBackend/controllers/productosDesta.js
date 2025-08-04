078ef96757c0c27d06c874ac8b0fd63c21c3840f
Marcus21RN <0323105847@ut-tijuana.edu.mx>
2025-08-04 07:43:11 -0700

    Dimiss

--- server/clientBackend/controllers/productosDesta.js
+++ server/clientBackend/controllers/productosDesta.js
@@ -5,7 +5,7 @@ const CATEGORIAS_DESTACADAS = [
   { categoria: "bebidas", imagen: "https://images.unsplash.com/photo-1620219365994-f443a86ea626?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   { categoria: "cocteles", imagen: "https://images.unsplash.com/photo-1566417713940-fe7c737a9ef2?q=80&w=2029&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   { categoria: "alimentos", imagen: "https://images.unsplash.com/photo-1565060299172-42f7895549f0?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { categoria: "snacks", imagen: "https://images.unsplash.com/photo-1730698394350-76b6f0ac8e03?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  { categoria: "snacks", imagen: "https://png.pngtree.com/background/20230321/original/pngtree-crispy-potato-chips-in-a-small-cup-picture-image_2153765.jpg" }
 ];
 
 export const obtenerDestacadosHome = async (req, res) => {
